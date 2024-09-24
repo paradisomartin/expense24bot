1d574cfec0714db4e36942df8943593884b304a6
Martin <martinparadiso024@gmail.com>
2024-09-24 16:55:42 -0300

    error handling

--- connector_service/telegramBot.js
+++ connector_service/telegramBot.js
@@ -13,20 +13,35 @@ const getUsernameOrError = (msg) => {
 
 const sendMessageToBotService = async (username, messageText) => {
   console.log(`Sending request to Bot Service at: ${BOT_SERVICE_URL}`);
-  const response = await axios.post(BOT_SERVICE_URL, {
-    message: messageText,
-    telegram_id: username,
-  });
-  return response.data.response;
+  try {
+    const response = await axios.post(BOT_SERVICE_URL, {
+      message: messageText,
+      telegram_id: username,
+    });
+    return response.data.response;
+  } catch (error) {
+    if (error.response && error.response.status === 403) {
+      throw new Error("Unauthorized user");
+    }
+    throw error;
+  }
 };
 
 const handleError = async (chatId, error) => {
-  console.error("Error:", error.response ? error.response.data : error.message);
+  console.error("Error:", error.message);
   console.error("Full error object:", JSON.stringify(error, null, 2));
 
-  let errorMessage = "Sorry, there was an error processing your message.";
-  if (error.message === "No username") {
-    errorMessage = "You must set up a username in Telegram to use this Bot.";
+  let errorMessage;
+  switch (error.message) {
+    case "No username":
+      errorMessage = "You must set up a username in Telegram to use this Bot.";
+      break;
+    case "Unauthorized user":
+      errorMessage =
+        "You are not authorized to use this Bot. Please contact the administrator.";
+      break;
+    default:
+      errorMessage = "Sorry, there was an error processing your message.";
   }
 
   await bot.sendMessage(chatId, errorMessage);
